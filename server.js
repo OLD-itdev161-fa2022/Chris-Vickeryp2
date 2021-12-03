8820105206e3210624fa14a4de8607cfed8bee85
Chris Vickery <vickeryc@gmail.com>
2021-12-02 20:02:21 -0600

    all stuff added form act 3, still rote back end set up

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import connectDatabase from "./config/db";
+import { check, validationResult } from "express-validator";
 
 // init express
 const app = express();
@@ -7,9 +8,40 @@ const app = express();
 //connect database
 connectDatabase();
 
+//config middleware
+app.use(express.json({ extended: false }));
+
 //api endpoints
+/**
+ * @route Get
+ * @desc test end point
+ */
 app.get("/", (req, res) =>
   res.send("http get request send to root api endoint")
+);
+
+/**
+ * @route Post api/user
+ * @desc Register user
+ */
+app.post(
+  "/api/users",
+  [
+    check("name", "Pls enter your name").not().isEmpty(),
+    check("email", "Please enter a valid email adress").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 1 or more charcters"
+    ).isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    } else {
+      return res.send(req.body);
+    }
+  }
 );
 
 //lissenter
