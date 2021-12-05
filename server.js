a0ad073ad0f3e41988408141c83764895c7e332b
Chris Vickery <vickeryc@gmail.com>
2021-12-05 00:33:00 -0600

    stuff form act 5

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ import express from "express";
 import connectDatabase from "./config/db";
 import { check, validationResult } from "express-validator";
 import cors from "cors";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import config from "config";
+import User from "./models/User";
 
-// init express
+// Initialize express application
 const app = express();
 
-//connect database
+// Connect database
 connectDatabase();
 
-//config middleware
+// Configure Middleware
 app.use(express.json({ extended: false }));
 app.use(
   cors({
@@ -17,39 +21,83 @@ app.use(
   })
 );
 
-//api endpoints
+// API endpoints
 /**
- * @route Get
- * @desc test end point
+ * @route GET /
+ * @desc Test endpoint
  */
 app.get("/", (req, res) =>
-  res.send("http get request send to root api endoint")
+  res.send("http get request sent to root api endpoint")
 );
 
+app.get("/api/", (req, res) => res.send("http get request sent to api"));
+
 /**
- * @route Post api/user
+ * @route POST api/users
  * @desc Register user
  */
 app.post(
   "/api/users",
   [
-    check("name", "Pls enter your name").not().isEmpty(),
-    check("email", "Please enter a valid email adress").isEmail(),
+    check("name", "Please enter your name").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
     check(
       "password",
-      "Please enter a password with 1 or more charcters"
-    ).isLength({ min: 5 }),
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     } else {
-      return res.send(req.body);
+      const { name, email, password } = req.body;
+      try {
+        // Check if user exists
+        let user = await User.findOne({ email: email });
+        if (user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: "User already exists" }] });
+        }
+
+        // Create a new user
+        user = new User({
+          name: name,
+          email: email,
+          password: password,
+        });
+
+        // Encrypt the password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        // Save to the db and return
+        await user.save();
+
+        // Generate and return a JWT token
+        const payload = {
+          user: {
+            id: user.id,
+          },
+        };
+
+        jwt.sign(
+          payload,
+          config.get("jwtSecret"),
+          { expiresIn: "10hr" },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token: token });
+          }
+        );
+      } catch (error) {
+        res.status(500).send("Server error");
+      }
     }
   }
 );
 
-//lissenter
-let port = 5000;
-app.listen(port, () => console.log(`express running on port ${port}`));
+// Connection listener
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
